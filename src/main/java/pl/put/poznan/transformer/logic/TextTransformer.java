7131f5486ecac082101936d2b931911a99f45b53
BlackRoosie <73405676+BlackRoosie@users.noreply.github.com>
2020-12-20 21:03:07 +0100

    Add upper, lower, capitalize and reverse methods

--- src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
@@ -15,4 +15,53 @@ public class TextTransformer {
         // of course, normally it would do something based on the transforms
         return text.toUpperCase();
     }
+}package pl.put.poznan.transformer.logic;
+
+import org.apache.commons.text.WordUtils;
+
+import java.util.Locale;
+
+/**
+ * This is just an example to show that the logic should be outside the REST service.
+ */
+public class TextTransformer {
+
+    private final String[] transforms;
+
+    public TextTransformer(String[] transforms){
+        this.transforms = transforms;
+    }
+
+    public String upper(String text){
+        // of course, normally it would do something based on the transforms
+        return text.toUpperCase();
+    }
+
+    public String lower(String text){
+        return text.toLowerCase();
+    }
+
+    public String capitalize(String text){
+        return WordUtils.capitalizeFully(text);
+    }
+
+    public String reverse(String text) {
+        boolean[] upperCase = new boolean[text.length()];
+
+        for (int i = 0; i < text.length(); i++)
+            upperCase[i] = Character.isUpperCase(text.charAt(i));
+
+        text.toLowerCase();
+        String output = "";
+        for (int i = text.length() - 1; i >= 0; i--) {
+            if (upperCase[text.length() - 1 - i])
+                output += Character.toUpperCase(text.charAt(i));
+            else
+                output += text.charAt(i);
+        }
+
+        return output;
+    }
+
 }
+
