6ee036840d2c60f8555a303cbb3a81431b47f62f
Naturzhu <49533956+Naturzhu@users.noreply.github.com>
2021-01-26 14:38:16 +0100

    mocki v3

--- src/test/java/pl/put/poznan/transformer/logic/TextTransformerControllerTest.java
+++ src/test/java/pl/put/poznan/transformer/logic/TextTransformerControllerTest.java
@@ -1,11 +1,15 @@
 package pl.put.poznan.transformer.logic;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pl.put.poznan.transformer.rest.TextTransformerController;
+
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class TextTransformerControllerTest {
     private TextTransformer transformer = mock(TextTransformer.class);
-
+    @Test
     public String testPost() {
         String text = "test";
         String[] transforms = {"upper"};
